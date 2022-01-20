304121824ae6c7354acbe7920a6ffd7da42f7152
Tatiana Pavlova <pavlova.tat.ole@gmail.com>
2022-01-20 13:23:53 +0300

    refactor: move sign-in sign-up routers to separate file

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
-const { celebrate, Joi, errors } = require('celebrate');
+const { errors } = require('celebrate');
 const cookieParser = require('cookie-parser');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 const auth = require('./middlewares/auth');
-const { createUser, login, signout } = require('./controllers/users');
+const routerUserAccount = require('./routes/userAccount');
 const routerUser = require('./routes/users');
 const routerMovie = require('./routes/movies');
 const NotFoundError = require('./errors/NotFoundError');
@@ -25,22 +25,7 @@ app.use(express.json());
 
 app.use(requestLogger);
 
-app.post('/signup', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-    name: Joi.string().required().min(2).max(30),
-  }),
-}), createUser);
-
-app.post('/signin', celebrate({
-  body: Joi.object().keys({
-    email: Joi.string().required().email(),
-    password: Joi.string().required().min(8),
-  }),
-}), login);
-
-app.post('/signout', signout);
+app.use(routerUserAccount);
 
 app.use(auth);
 app.use(routerUser);
---
+++ routes/userAccount.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const { celebrate, Joi } = require('celebrate');
+const { createUser, login, signout } = require('../controllers/users');
+
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+    name: Joi.string().required().min(2).max(30),
+  }),
+}), createUser);
+
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required().min(8),
+  }),
+}), login);
+
+router.post('/signout', signout);
+
+module.exports = router;
