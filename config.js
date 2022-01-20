1cec65b1728b37aa4216cd1649e86c7b0eed39c7
Tatiana Pavlova <pavlova.tat.ole@gmail.com>
2022-01-21 02:24:22 +0300

    fix

--- config.js
+++ config.js
@@ -1,8 +1,8 @@
-require('dotenv').config();
+// require('dotenv').config();
 
-const { NODE_ENV, JWT_SECRET, MONGO_URL } = process.env;
+// const { NODE_ENV, JWT_SECRET, MONGO_URL } = process.env;
 
-module.exports = {
-  JWT_SECRET: NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
-  MONGO_URL: NODE_ENV === 'production' ? MONGO_URL : 'mongodb://localhost:27017/moviesdb',
-};
+// module.exports = {
+//   JWT_SECRET: NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+//   MONGO_URL: NODE_ENV === 'production' ? MONGO_URL : 'mongodb://localhost:27017/moviesdb',
+// };
