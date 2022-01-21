575d7d03554cb0df19a491092d1ea4358c2f0041
Tatiana Pavlova <pavlova.tat.ole@gmail.com>
2022-01-21 14:27:37 +0300

    refactor: rename db

--- config.js
+++ config.js
@@ -4,5 +4,5 @@ const { NODE_ENV, JWT_SECRET, MONGO_URL } = process.env;
 
 module.exports = {
   JWT_SECRET: NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
-  MONGO_URL: NODE_ENV === 'production' ? MONGO_URL : 'mongodb://localhost:27017/moviesdevdb',
+  MONGO_URL: NODE_ENV === 'production' ? MONGO_URL : 'mongodb://localhost:27017/moviesdb',
 };
